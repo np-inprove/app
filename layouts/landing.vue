5b1049d3473de4e3ef380c5c2836a0a0eb62723f
Qin Guan <qinguan20040914@gmail.com>
2023-05-04 11:47:11 +0800

    feat: wip page layouts

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-    iNProve
+    <div container px-4 py-2 mx-auto>
+      <img h-8 src="~/assets/logos/small.webp" alt="iNProve">
+    </div>
   </header>
   <main>
     <slot />
--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,38 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetUno({
+      attributifyPseudo: true,
+    }),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      extraProperties: {
+        'color': 'inherit',
+        // Avoid crushing of icons in crowded situations
+        'min-width': '1.2em',
+      },
+    }),
+    presetTypography(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Inter',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 })

45e98c6967cd1f48ca57c6a207e3805ac28a9b2d
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-05-04 17:24:21 +0800

    Website Header (Computer)
    Created the Login Page with default template and modified the landing page to include the header of the website

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -2,8 +2,23 @@
   <div h-full>
     <header>
       <div container mx-auto>
-        <div px5 py3>
+        <div px5 py3 flex>
           <img h-8 src="~/assets/logos/small.webp" alt="iNProve">
+          <div ml-auto>
+            <NuxtLink to="/about">
+              About Us
+            </NuxtLink>
+          </div>
+          <div px5>
+            <NuxtLink to="/faq">
+              Support
+            </NuxtLink>
+          </div>
+          <div>
+            <NuxtLink to="/login">
+              Login
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </header>

286d8f818f48eb4793043d8e8ac19cf69269d6e1
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-05-08 13:39:24 +0800

    Created Base Login Page
    Styled the Login Page Yet to connect to back end
    
    TODO
    + Change to inline styling
    + Connect to backend

--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,62 @@
 definePageMeta({
   layout: 'landing',
 })
+
+const forminfo = reactive({
+  email: '',
+  password: '',
+})
+
+function login() {
+  console.log(forminfo)
+  forminfo.email = ''
+  forminfo.password = ''
+}
+
+const errormessage = 'hi'
 </script>
 
 <template>
-  <div>
-    login page
+  <div class="centralise" flex justify-center>
+    <div class="rcorners" bg-primary-container rounded-3xl>
+      <h1 text-15 pb-5>Login</h1>
+      <form flex flex-col>
+        <input id="email" v-model="forminfo.email" type="text" name="email" placeholder="Email" rounded-3xl class="formfield" required>
+        <input id="password" v-model="forminfo.password" type="password" name="password" placeholder="Password" rounded-3xl class="formfield" required>
+        <div flex justify-center>
+          <button type="submit" btn-filled w-fit m-5 @click.prevent="login">
+            Login
+          </button>
+        </div>
+      </form>
+      <div>
+        <p>
+          {{ errormessage }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+p {
+  color: red;
+  font-weight: bold;
+}
+
+.centralise {
+  text-align: center;
+  margin: 25px;
+}
+
+.rcorners {
+  padding: 20px;
+  width: 60%;
+}
+
+.formfield {
+  margin: 10px;
+  padding: 10px;
+  text-align: center;
+}
+</style>

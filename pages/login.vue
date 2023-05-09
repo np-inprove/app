8ef3680f2586a71f4ad2e662d77f83700126c44d
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-05-09 11:34:01 +0800

    Login Front-End Completed
    UI for Mobile Fixed
    Changed to inline Styling
    
    TODO
    + Connect to Back-End Database
    + Connect to Microsoft / Google Login (OPTIONAL)

--- pages/login.vue
+++ pages/login.vue
@@ -8,56 +8,40 @@ const forminfo = reactive({
   password: '',
 })
 
+const errormessage = ref()
+
 function login() {
-  console.log(forminfo)
+  // errormessage.value = 'hi'
+  // console.log(forminfo)
   forminfo.email = ''
   forminfo.password = ''
 }
-
-const errormessage = 'hi'
 </script>
 
 <template>
-  <div class="centralise" flex justify-center>
-    <div class="rcorners" bg-primary-container rounded-3xl>
+  <div flex justify-center text-center>
+    <div bg-primary-container rounded-3xl p-10 class="w-full md:w-3/5" h-fit>
       <h1 text-15 pb-5>Login</h1>
-      <form flex flex-col>
-        <input id="email" v-model="forminfo.email" type="text" name="email" placeholder="Email" rounded-3xl class="formfield" required>
-        <input id="password" v-model="forminfo.password" type="password" name="password" placeholder="Password" rounded-3xl class="formfield" required>
+      <form flex flex-col @submit.prevent="login">
         <div flex justify-center>
-          <button type="submit" btn-filled w-fit m-5 @click.prevent="login">
+          <input id="email" v-model="forminfo.email" type="email" name="email" placeholder="Email" rounded-3xl p-5 m-3
+            mx-0 md:mx-5 w-full required>
+        </div>
+        <div flex justify-center>
+          <input id="password" v-model="forminfo.password" type="password" name="password" placeholder="Password"
+            rounded-3xl p-5 m-3 mx-0 md:mx-5 w-full required>
+        </div>
+        <div flex justify-center>
+          <button type="submit" btn-filled w-fit m-5>
             Login
           </button>
         </div>
       </form>
       <div>
-        <p>
+        <p text-error font-bold>
           {{ errormessage }}
         </p>
       </div>
     </div>
   </div>
 </template>
-
-<style>
-p {
-  color: red;
-  font-weight: bold;
-}
-
-.centralise {
-  text-align: center;
-  margin: 25px;
-}
-
-.rcorners {
-  padding: 20px;
-  width: 60%;
-}
-
-.formfield {
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-}
-</style>

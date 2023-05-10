4965cc954288a202706680c1c22790d95a4df27a
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-05-10 12:30:29 +0800

    feat: Use material design for input field

---
+++ components/Input.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps({
+  label: String,
+  type: String,
+  name: String,
+  required: Boolean,
+  modelValue: String,
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="w-72">
+    <div class="relative h-12 min-w-[200px] w-full">
+      <input
+        peer
+        class="h-full w-full border border-outline border-t-transparent rounded-[7px] bg-transparent px-3 py-2.5 font-sans text-base font-normal outline-0 outline transition-all focus:border-2 placeholder-shown:border focus:border-primary placeholder-shown:border-outline placeholder-shown:border-outline focus:border-t-transparent disabled:bg-primary-container/12 focus:outline-0"
+        :type="props.type"
+        :name="props.name"
+        :value="modelValue"
+        placeholder=" "
+        :required="props.required"
+        @input="$emit('update:modelValue', $event.target.value)"
+      >
+      <label
+        class="peer-placeholder-shown:text-border-outline peer-disabled:peer-placeholder-shown:text-border-outline pointer-events-none absolute left-0 h-full w-full flex select-none text-[11px] font-normal leading-tight transition-all after:pointer-events-none before:pointer-events-none -top-1.5 after:ml-1 after:mt-[6.5px] before:mr-1 before:mt-[6.5px] after:box-border before:box-border after:block before:block after:h-1.5 after:w-2.5 before:h-1.5 before:w-2.5 after:flex-grow after:border-r after:border-t before:border-l before:border-t after:border-outline before:border-outline after:rounded-tr-md before:rounded-tl-md peer-focus:text-[11px] peer-placeholder-shown:text-base peer-focus:leading-tight peer-placeholder-shown:leading-[3.75] peer-disabled:text-transparent peer-focus:text-primary after:transition-all before:transition-all after:content-space before:content-space peer-focus:after:border-r-2 peer-focus:after:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-t-2 peer-disabled:after:border-transparent peer-disabled:before:border-transparent peer-focus:after:border-primary peer-focus:before:border-primary peer-placeholder-shown:after:border-transparent peer-placeholder-shown:before:border-transparent"
+      >
+        {{ props.label }}
+      </label>
+    </div>
+  </div>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -11,7 +11,8 @@ const forminfo = reactive({
 const errormessage = ref()
 
 function login() {
-  // errormessage.value = 'hi'
+  // TODO: Connect to backend
+  errormessage.value = 'hi'
   // console.log(forminfo)
   forminfo.email = ''
   forminfo.password = ''
@@ -20,22 +21,18 @@ function login() {
 
 <template>
   <div flex justify-center text-center>
+    <!-- TODO: Waiting for colours to finalise -->
+    <!-- <div class="w-full md:w-3/5" h-fit rounded-3xl bg-surface-container-lowest p-10> -->
     <div class="w-full md:w-3/5" h-fit rounded-3xl bg-primary-container p-10>
       <h1 pb-5 text-15>
         Login
       </h1>
       <form flex flex-col @submit.prevent="login">
         <div flex justify-center>
-          <input
-            id="email" v-model="forminfo.email" type="email" name="email" placeholder="Email"
-            required m-3 mx-0 w-full rounded-3xl p-5 md:mx-5
-          >
+          <Input v-model="forminfo.email" label="Email" type="email" name="email" :required="true" m-3 mx-0 w-full md:mx-6 />
         </div>
         <div flex justify-center>
-          <input
-            id="password" v-model="forminfo.password" type="password" name="password" placeholder="Password"
-            required m-3 mx-0 w-full rounded-3xl p-5 md:mx-5
-          >
+          <Input v-model="forminfo.password" label="Password" type="password" name="password" :required="true" m-3 mx-0 w-full md:mx-6 />
         </div>
         <div flex justify-center>
           <button type="submit" m-5 w-fit btn-filled>

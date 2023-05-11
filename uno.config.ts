c5e18c3d706ce4d9165f82f60f208d1bcf98dace
Sherlena Ngui <66990228+C4RR0T02@users.noreply.github.com>
2023-05-11 10:04:00 +0800

    fix: colour fix
    Co-authored-by: Qin Guan <qinguan20040914@gmail.com>

--- uno.config.ts
+++ uno.config.ts
@@ -77,10 +77,7 @@ export default defineConfig({
       shadow: 'hsl(var(--c-shadow))',
       logo: {
         i: 'hsl(var(--c-logo-i))',
-        np: {
-          dark: 'hsl(var(--c-logo-np-dark))',
-          light: 'hsl(var(--c-logo-np-light))',
-        },
+        np: 'hsl(var(--c-logo-np))',
         rv: 'hsl(var(--c-logo-rv))',
         oe: 'hsl(var(--c-logo-oe))',
       },

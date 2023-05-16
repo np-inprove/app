b0d9f7370abbceedc7cae004f5e724b4d5d8e7fb
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-05-16 22:01:51 +0800

    feat: FAQ Page
    Nuxt Linking to various pages
    
    TODO
    + Implement Search Bar

--- pages/faqs.vue
+++ pages/faqs.vue
@@ -2,10 +2,50 @@
 definePageMeta({
   layout: 'landing',
 })
+
+interface Faq {
+  text: string
+  link: string
+}
+
+const faqs = [
+  {
+    text: 'How do I reset my account password?',
+    link: '/login',
+  },
+  {
+    text: 'How do I turn off notifications?',
+    link: '',
+  },
+  {
+    text: 'How do I upload a file to the forum?',
+    link: '',
+  },
+  {
+    text: 'How do I create an event for a group?',
+    link: '',
+  },
+  {
+    text: 'How do I redeem reward vouchers?',
+    link: '',
+  },
+  {
+    text: 'How do I reset my account password?',
+    link: '',
+  },
+] satisfies Faq[]
 </script>
 
 <template>
-  <div>
-    FAQ page
+  <div flex flex-col items-center>
+    <h1 p-5 text-center text-3xl>
+      Frequently Asked Questions
+    </h1>
+    <div>
+      Search Bar Here
+    </div>
+    <div class="w-full md:w-3/5" grid grid-cols-1 mt-5 gap-3 overflow-hidden border-2 border-outline rounded-3xl pb-3 text-left divide-y-2 divide-outline>
+      <FAQRedirect v-for="faq in faqs" v-bind="faq" :key="faq.text" />
+    </div>
   </div>
 </template>

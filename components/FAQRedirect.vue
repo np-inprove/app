ae7037e1e90b64e2d868201180f1bee9ca3c176e
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-05-16 22:01:51 +0800

    feat: FAQ Page
    Nuxt Linking to various pages
    
    TODO
    + Implement Search Bar

---
+++ components/FAQRedirect.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String,
+  link: String,
+})
+</script>
+
+<template>
+  <NuxtLink px-3 pt-3 :to="props.link">
+    {{ props.text }}
+  </NuxtLink>
+</template>

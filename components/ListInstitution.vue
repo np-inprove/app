bbd7cda0f0c569f7b409421a8d4968e5212f4578
Yun-E <tye9793@gmail.com>
2023-05-18 00:05:23 +0800

    feat: view and edit institutions pages

---
+++ components/ListInstitution.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String,
+})
+</script>
+
+<template>
+  <div flex-1 rounded-md bg-surface-container p-3>
+    <div>
+      <p>
+        {{ props.name }}
+      </p>
+    </div>
+  </div>
+</template>
--- pages/admin.vue
+++ pages/admin.vue
@@ -1,12 +1,36 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'landing',
+  layout: 'app',
   middleware: 'god-mode',
 })
+
+const institutions = [
+  {
+    name: 'Ngee Ann Polytechnic',
+  },
+  {
+    name: 'Gardens by the Bay',
+  },
+  {
+    name: 'Cloud Forest',
+  },
+]
 </script>
 
 <template>
   <div>
     iNProve project admin page
+    <div grid="~ cols-1 md:cols-3 gap-2">
+      <div grid="~ gap-2">
+        <ListInstitution v-for="author in institutions" v-bind="author" :key="author.name" />
+      </div>
+      <div grid="col-span-2">
+        <div h-32 flex rounded-md bg-tertiary>
+          <p align-middle text-white>
+            Statistics
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>

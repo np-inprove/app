64bdc3369e69316e132fd5c857957a99a3d34262
Qin Guan <qinguan20040914@gmail.com>
2023-05-22 01:10:31 +0800

    fix: test

--- plugins/api.ts
+++ plugins/api.ts
@@ -1,9 +1,11 @@
 export default defineNuxtPlugin(() => {
   const {
     public: {
-      apiUrl = (globalThis as any).NUXT_PUBLIC_API_URL ?? 'http://localhost:3333',
+      apiUrl = (globalThis as any).NUXT_PUBLIC_API_URL,
     },
   } = useRuntimeConfig()
+
+  console.log(globalThis)
 
   const api = $fetch.create({
     baseURL: apiUrl,

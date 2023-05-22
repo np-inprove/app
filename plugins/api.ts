240bff4bd9d15fa6f33a78a12a661112214a3bbf
Qin Guan <qinguan20040914@gmail.com>
2023-05-22 08:55:53 +0800

    fix: help

--- plugins/api.ts
+++ plugins/api.ts
@@ -5,7 +5,7 @@ export default defineNuxtPlugin(() => {
     },
   } = useRuntimeConfig()
 
-  console.log(globalThis.NUXT_PUBLIC_API_URL)
+  console.log('help', globalThis.NUXT_PUBLIC_API_URL)
 
   const api = $fetch.create({
     baseURL: apiUrl,

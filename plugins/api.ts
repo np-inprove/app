836d18ddb797405186c1d01a86020558b8f89ecd
Qin Guan <qinguan20040914@gmail.com>
2023-05-22 14:49:36 +0800

    fix: ?!?!

--- plugins/api.ts
+++ plugins/api.ts
@@ -2,6 +2,8 @@ export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
 
   const api = (() => {
+    console.log('creating plugin', process.client)
+
     if (process.client) {
       return $fetch.create({
         baseURL: config.public.apiUrl,
@@ -10,6 +12,8 @@ export default defineNuxtPlugin(() => {
     }
 
     const e = useRequestEvent()
+
+    console.log(JSON.stringify(e.context.cloudflare))
 
     const baseURL = e.context.cloudflare ? e.context.cloudflare.NUXT_PUBLIC_API_URL : config.public.apiUrl
 

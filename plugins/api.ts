da47ecc9d7dd65cbff8c966c3661c5bcd426b6c3
Qin Guan <qinguan20040914@gmail.com>
2023-05-22 14:54:41 +0800

    fix: blindness xv

--- plugins/api.ts
+++ plugins/api.ts
@@ -2,8 +2,6 @@ export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
 
   const api = (() => {
-    console.log('creating plugin', process.client)
-
     if (process.client) {
       return $fetch.create({
         baseURL: config.public.apiUrl,
@@ -13,9 +11,7 @@ export default defineNuxtPlugin(() => {
 
     const e = useRequestEvent()
 
-    console.log(JSON.stringify(e.context.cloudflare))
-
-    const baseURL = e.context.cloudflare ? e.context.cloudflare.NUXT_PUBLIC_API_URL : config.public.apiUrl
+    const baseURL = e.context.cloudflare ? e.context.cloudflare.env.NUXT_PUBLIC_API_URL : config.public.apiUrl
 
     return $fetch.create({
       baseURL,

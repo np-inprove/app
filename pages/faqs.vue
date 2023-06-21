ef7dc230d307ca1bc170ee1e2f3b44d2fb464a52
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2023-06-21 12:28:57 +0800

    feat: Update FAQ to use Accordion

--- pages/faqs.vue
+++ pages/faqs.vue
@@ -1,50 +1,53 @@
 <script setup lang="ts">
+import Accordion from 'primevue/accordion'
+import AccordionTab from 'primevue/accordiontab'
+
 definePageMeta({
   layout: 'landing',
 })
 
 interface Faq {
-  text: string
-  link: string
+  title: string
+  content: string
 }
 
 const faqs = [
   {
-    text: 'How do I reset my account password?',
-    link: '/faq-pages/reset-password',
+    title: 'How do I reset my account password?',
+    content: '/faq-pages/reset-password',
   },
   {
-    text: 'How do I turn off notifications?',
-    link: '/faq-pages/off-notifications',
+    title: 'How do I turn off notifications?',
+    content: '/faq-pages/off-notifications',
   },
   {
-    text: 'How do I upload a file to the forum?',
-    link: '/faq-pages/uploading-file',
+    title: 'How do I upload a file to the forum?',
+    content: '/faq-pages/uploading-file',
   },
   {
-    text: 'How do I create an event for a group?',
-    link: '/faq-pages/event-creation',
+    title: 'How do I create an event for a group?',
+    content: '/faq-pages/event-creation',
   },
   {
-    text: 'How do I redeem reward vouchers?',
-    link: '/faq-pages/prize-redemption',
+    title: 'How do I redeem reward vouchers?',
+    content: '/faq-pages/prize-redemption',
   },
   {
-    text: 'Am I able to change my pet?',
-    link: '/faq-pages/pets-customisation',
+    title: 'Am I able to change my pet?',
+    content: '/faq-pages/pets-customisation',
   },
 ] satisfies Faq[]
 </script>
 
 <template>
-  <div flex flex-col items-center>
+  <div w-full items-center>
     <h1 p-5 text-center text-3xl>
       Frequently Asked Questions
     </h1>
-    <div class="w-full md:w-3/5" grid grid-cols-1 my-5 gap-3 overflow-hidden border-2 border-outline rounded-3xl pb-3 text-left divide-y-2 divide-outline>
-      <NuxtLink v-for="faq in faqs" :key="faq.text" px-3 pt-3 :to="faq.link">
-        {{ faq.text }}
-      </NuxtLink>
-    </div>
+    <Accordion :active-index="0">
+      <AccordionTab v-for="faq in faqs" :key="faq.title" :header="faq.title">
+        <p>{{ faq.content }}</p>
+      </AccordionTab>
+    </Accordion>
   </div>
 </template>

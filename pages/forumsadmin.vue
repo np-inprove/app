3085cbf2dce178cfb77350250dae4c03949dda5f
Yun-E <tye9793@gmail.com>
2023-07-10 23:50:29 +0800

    feat: create forums page layout

---
+++ pages/forumsadmin.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+import Menu from 'primevue/menu'
+
+import Toast from 'primevue/toast'
+import { ref } from 'vue'
+
+definePageMeta({
+  layout: 'app',
+})
+
+const router = useRouter()
+
+const forum_groups = [
+  {
+    name: 'NS CSF01',
+    shortName: 'NSSF_01',
+    description: 'NS group for CSF01',
+  },
+  {
+    name: 'NS CSF02',
+    shortName: 'NSSF_01',
+    description: 'NS group for CSF02',
+  },
+]
+
+const items = ref([
+  {
+    label: 'NS CSF01',
+    to: `/forumsadmin/${forum_groups[1].shortName}`,
+  },
+  {
+    label: 'NS CSF02',
+    to: `/forumsadmin/${forum_groups[1].shortName}`,
+  },
+])
+
+function toCreate() {
+  router.push('/forumsadmin')
+}
+</script>
+
+<template>
+  <Toast />
+
+  <div flex flex-col gap-8 lg:flex-row>
+    <div>
+      <h2 text-2xl font-semibold>
+        Group Forums
+      </h2>
+
+      <Menu :model="items">
+        <template #end>
+          <Button text w-full @click="toCreate">
+            New
+          </Button>
+        </template>
+      </Menu>
+    </div>
+
+    <div flex-1>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+:deep(a.router-link-active) {
+    background-color: var(--highlight-bg);
+    color: var(--highlight-text-color);
+    --at-apply: font-bold;
+}
+
+:deep(a.router-link-active:hover) {
+    background-color: var(--highlight-bg);
+    color: var(--highlight-text-color);
+}
+
+:deep(a.router-link-active>.menu-icon i) {
+    color: var(--highlight-text-color);
+}
+</style>
---
+++ pages/forumsadmin/index.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+
+import type { ValidationError } from '~/utils/error'
+
+definePageMeta({
+  layout: 'app',
+})
+
+const initFormData = {
+  name: '',
+  shortName: '',
+  description: '',
+  isLoading: false,
+  error: {},
+}
+
+const formData = ref<{
+  name: string
+  shortName: string
+  description: string
+  isLoading: boolean
+  error: ValidationError
+}>(initFormData)
+
+const forum_groups = [
+  {
+    name: 'NS CSF01',
+    shortName: 'NSSF_01',
+    description: 'NS group for CSF01',
+  },
+  {
+    name: 'NS CSF02',
+    shortName: 'NSSF_01',
+    description: 'NS group for CSF02',
+  },
+]
+</script>
+
+<template>
+  <h3 text-lg font-semibold>
+    <span>
+      Create Forum
+    </span>
+  </h3>
+
+  <div mt-5>
+    <form @submit.prevent="">
+      <div flex flex-col gap-2>
+        <div>
+          <span class="p-float-label">
+            <InputText id="name" v-model="formData.name" type="text" required class="w-full" />
+            <label for="name">Name</label>
+          </span>
+          <small class="p-error">{{ formData.error?.fields?.name || '&nbsp;' }}</small>
+        </div>
+
+        <div>
+          <span class="p-float-label">
+            <InputText id="shortName" v-model="formData.shortName" type="text" required class="w-full" />
+            <label for="shortName">Short name</label>
+          </span>
+          <small class="p-error">{{ formData.error?.fields?.shortName || '&nbsp;' }}</small>
+        </div>
+
+        <div>
+          <span class="p-float-label">
+            <InputText id="description" v-model="formData.description" type="text" required class="w-full" />
+            <label for="description">Description</label>
+          </span>
+          <small class="p-error">{{ formData.error?.fields?.description || '&nbsp;' }}</small>
+        </div>
+
+        <div flex gap-3>
+          <Button type="submit">
+            Create
+          </Button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
